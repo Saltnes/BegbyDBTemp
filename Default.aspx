6bbd177b3b648a15047dd7a36849d85614589e3f
Saltnes <robinbegb@gmail.com>
2022-09-30 11:01:01 +0200

    commit

---
+++ Default.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="BegbyDBTemp._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <h1>Database Oppslag</h1>
+    </div>
+
+    <div class="row">
+        <asp:TextBox ID="TextBoxSearch" runat="server"></asp:TextBox>
+        <asp:Button ID="ButtonGetByName" runat="server" Text="Button"/>
+        <br/>
+        <br/>
+        <asp:GridView class="table" ID="GridView1" runat="server"></asp:GridView>
+    </div>
+
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BegbyDBTemp
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            BindGrid();
+        }
+
+        protected void ButtonGetByName_Click(object sender, EventArgs e)
+        {
+            GridView1.DataSource = GetByName(TextBoxSearch.Text);
+            GridView1.DataBind();
+        }
+
+        private DataTable GetByName(string name)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["ConnAir"].ConnectionString;
+            DataTable dt = new DataTable();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM dbo.People WHERE first_name=@name", conn);
+                cmd.CommandType = CommandType.Text;
+
+                // param = new SqlParameter("@name", SqlDbType.NVarChar);
+                // param.Value = name;
+                // cmd.Parameters.Add(param);
+
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                dt.Load(reader);
+
+                reader.Close();
+                conn.Close();
+            }
+
+            return dt;
+        }
+
+
+        private void BindGrid()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["ConnAir"].ConnectionString;
+            DataTable dt = new DataTable();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM dbo.People", conn);
+                cmd.CommandType = CommandType.Text;
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                dt.Load(reader);
+
+                reader.Close();
+                conn.Close();
+            }
+
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+    }
+}
